0f4ffeb5524a533847e21b3556d52b84fedd9042
Loanid <anmibue@googlemail.com>
2015-11-20 21:08:30 +0100

    Work continues

--- Noises/GeneratorTest/Controller.xaml.cs
+++ Noises/GeneratorTest/Controller.xaml.cs
@@ -17,11 +17,61 @@ namespace GeneratorTest
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Controller : Window
     {
-        public MainWindow()
+        #region Properties
+
+        public int Width
+        {
+            get { return (int)GetValue(WidthProperty); }
+            set { SetValue(WidthProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Width.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty WidthProperty =
+            DependencyProperty.Register("Width", typeof(int), typeof(Controller), new PropertyMetadata(500));
+
+
+        public int Height
+        {
+            get { return (int)GetValue(HeightProperty); }
+            set { SetValue(HeightProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Height.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty HeightProperty =
+            DependencyProperty.Register("Height", typeof(int), typeof(Controller), new PropertyMetadata(500));
+
+
+        public int Seed
+        {
+            get { return (int)GetValue(SeedProperty); }
+            set { SetValue(SeedProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Seed.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty SeedProperty =
+            DependencyProperty.Register("Seed", typeof(int), typeof(Controller), new PropertyMetadata(new Random().Next()));
+
+        #endregion
+
+        public static RoutedCommand StartGenerator = new RoutedCommand();
+
+        public Controller()
         {
             InitializeComponent();
+            Init();
+        }
+
+        private void Init()
+        {
+            CommandBindings.Add(new CommandBinding(StartGenerator, StartGenerator_Executed));
+            DataContext = this;
+        }
+
+        public static void StartGenerator_Executed(object sender ,RoutedEventArgs e)
+        {
+
         }
     }
 }
