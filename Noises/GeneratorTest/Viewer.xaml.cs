73d96414838ed0cff6eef0a57d5bb3980469e5fb
Loanid <anmibue@googlemail.com>
2015-11-23 13:06:52 +0100

    Rearranges and Todo List

--- Noises/GeneratorTest/Viewer.xaml.cs
+++ Noises/GeneratorTest/Viewer.xaml.cs
@@ -11,7 +11,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
-namespace GeneratorTest
+namespace NoiseTest
 {
     /// <summary>
     /// Interaktionslogik für Viewer.xaml
